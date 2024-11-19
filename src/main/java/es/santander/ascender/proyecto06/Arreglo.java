4c690216168c9d1a469356d941db37154fbe8174
elsa <elsa@gmail.com>
2024-11-19 13:41:10 +0100

    Ultimo comiteo

--- src/main/java/es/santander/ascender/proyecto06/Arreglo.java
+++ src/main/java/es/santander/ascender/proyecto06/Arreglo.java
@@ -1,5 +1,24 @@
 package es.santander.ascender.proyecto06;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arreglo {
 
+    List<Perro> perrera = new ArrayList<>();
+
+    public List<String> localizaDuplicados (List<String> lista1,List<String> lista2) {
+        ArrayList<String> resultado = new ArrayList<>();
+        
+         for (String valor1: lista1) {
+            if(lista2.contains(valor1)) {
+                if(!resultado.contains(valor1)) {
+                resultado.add(valor1);
+            }
+        }
+     }
+    return resultado; 
+    
+
+    }
 }
